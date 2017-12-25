7dfd8416d6fc6f67a4febb1bf79f042e25a661c9
smile097575 <smile097575@gmail.com>
2017-12-25 15:28:54 +0800

    Add files via upload

---
+++ ｈｗ２.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ｈｗ２ {
+    public static void main(String[] args) {
+        Scanner scn =new Scanner(System.in);
+        int count[]=new int[52];
+        String str=scn.nextLine();
+        int a=str.length();
+        for (int i=0;i<a;i++){
+            int val=str.charAt(i)-'a';
+            if (val>=0&&val<26) {
+                count[val]++;
+            }else {
+                val=str.charAt(i)-'A';
+                if (val>=0&&val<26){
+                    count[26+val]++;
+                }
+            }
+        }
+        for (int i=0;i<52;i++){
+            if (count[i]>0){
+                if (i<26) {
+                    System.out.println((char)(97 + i) + ":" + count[i]);
+                }else {
+                    System.out.println((char)(65+i)+"i"+count[i]);
+                }
+            }
+        }
+    }
+}
+//文字出現次數
